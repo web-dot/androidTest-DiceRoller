4b38c2ef354807dd90069b788a788842431313eb
web-dot <fedev.sudipto@gmail.com>
2022-03-22 00:06:16 +0530

    dice roller app - saving button reference using findViewById() method and call lambda setOnClickListener{} on button reference to do something when the button is clicked

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.diceroller
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener {
+            val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
+            toast.show()
+        }
+    }
+}
