fa86b86f5f25c655d3f76e8433395bd97065182d
web-dot <fedev.sudipto@gmail.com>
2022-03-22 23:04:09 +0530

    created Dice class with roll() method | implemented diceRoll() method that creates a instance of a Dice and sets the text of the textView with the return of Dice.roll()

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -12,8 +12,24 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener {
-            val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
-            toast.show()
+            //Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show()
+            //val resultTextView: TextView = findViewById(R.id.textView)
+            //resultTextView.text = "6"
+            rollDice()
         }
+    }
+
+    fun rollDice(){
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = diceRoll.toString()
+    }
+}
+
+class Dice(val numSides: Int){
+
+    fun roll(): Int{
+        return (1..numSides).random()
     }
 }
